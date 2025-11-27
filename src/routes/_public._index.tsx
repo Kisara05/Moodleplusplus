8beed64586756b3888f43d71c2fdee29393a2ced
refute2change <madguyinthehouse@gmail.com>
2025-11-27 08:38:13 +0700

    fizzle fuddle

--- src/routes/_public._index.tsx
+++ src/routes/_public._index.tsx
@@ -1,39 +1,39 @@
-import { Footer } from "~/components/Footer";
-import { Header } from "~/components/Header";
-import { Hero } from "~/components/Hero";
-import { NewsSection } from "~/components/NewsSection";
-import { Sidebar } from "~/components/Sidebar";
-import type { Route } from "./+types/_public._index";
+// import { Footer } from "~/components/Footer";
+// import { Header } from "~/components/Header";
+// import { Hero } from "~/components/Hero";
+// import { NewsSection } from "~/components/NewsSection";
+// import { Sidebar } from "~/components/Sidebar";
+// import type { Route } from "./+types/_public._index";
 
-export function meta(_: Route.MetaArgs) {
-  return [
-    { title: "Moodle++" },
-    { name: "description", content: "Main page of Moodle++" },
-  ];
-}
+// export function meta(_: Route.MetaArgs) {
+//   return [
+//     { title: "Moodle++" },
+//     { name: "description", content: "Main page of Moodle++" },
+//   ];
+// }
 
-export default function Home() {
-  return (
-    <div
-      className="min-h-screen"
-      style={{
-        background:
-          "linear-gradient(135deg, rgb(208, 228, 255), rgb(255, 255, 255))",
-      }}
-    >
-      <Header />
-      <Hero />
-      <div className="container mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <div className="lg:col-span-2">
-            <NewsSection />
-          </div>
-          <div className="lg:col-span-1">
-            <Sidebar />
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div
+//       className="min-h-screen"
+//       style={{
+//         background:
+//           "linear-gradient(135deg, rgb(208, 228, 255), rgb(255, 255, 255))",
+//       }}
+//     >
+//       <Header />
+//       <Hero />
+//       <div className="container mx-auto px-4 py-8">
+//         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+//           <div className="lg:col-span-2">
+//             <NewsSection />
+//           </div>
+//           <div className="lg:col-span-1">
+//             <Sidebar />
+//           </div>
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
